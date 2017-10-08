a46ed6708f62e60deb081a43b608c9c4743e48fb
Michael Klopf <klopf.michael@gmail.com>
2017-10-08 21:44:06 +0200

    Tweaking components

--- src/components/CourseRegistration.tsx
+++ src/components/CourseRegistration.tsx
@@ -20,32 +20,31 @@ class CourseRegistration extends Component<CourseRegistrationProps> {
     this.register = this.register.bind(this);
   }
 
-  register() {
+  register(event: React.FormEvent<HTMLInputElement>): void {
+    event.preventDefault();
     // todo collect form data here
     let request = new CourseRegistrationRequestMessage(1, ['test-1']);
-    this.props.useCase.handle(request);
+    console.log(this.props.useCase.handle(request));
   }
 
   render() {
     return (
-      <div>
-        <form>
-          <h3>Enter your student identification:</h3> 
-          <label htmlFor="studentId">Student id</label>
-          <input
-            id="studentId"
-            type="text"
-            placeholder="Enter student id..."
-          />
-          <h3>Choose your courses:</h3> 
-          <ul>
-            {this.props.courses.map(function(course: Course){
-              return <CourseListItem courseName={course.name} key={course.code}/>;
-            })}
-          </ul>
-        </form>
-        <button type="button" onClick={this.register}>Register</button>
-      </div>
+      <form>
+        <h3>Enter your student identification:</h3> 
+        <label htmlFor="studentId">Student id</label>
+        <input
+          id="studentId"
+          type="text"
+          placeholder="Enter student id..."
+        />
+        <h3>Choose your courses:</h3> 
+        <ul>
+          {this.props.courses.map(function(course: Course){
+            return <CourseListItem courseName={course.name} key={course.code}/>;
+          })}
+        </ul>
+        <input type="submit" onClick={this.register} value="Register" />
+      </form>
     );
   }
 }
